2b5147ccb33760ef7d520fe12310d6365c98e357
sagayda <sagayda27@gmail.com>
2023-04-07 10:09:02 +0300

    Queue by index update

--- AlgorithmsLab4_WinForms/QueueByIndex.cs
+++ AlgorithmsLab4_WinForms/QueueByIndex.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 
 namespace AlgorithmsLab4_WinForms
 {
-    internal class ADTbyIndex
+    class QueueByIndex<T>
     {
+        public QueueByIndexElem<T> FirstElem { get; private set; }
+        public QueueByIndexElem<T> LastElem { get; private set; }
+        public QueueByIndex()
+        {
+            FirstElem = null;
+            LastElem = null;
+        }
+        public void Enqueue(T elem)
+        {
+            if(FirstElem == null) 
+            {
+                FirstElem = new QueueByIndexElem<T>(elem);
+                LastElem = FirstElem;
+                return;
+            }
+
+            LastElem.nextElement = new QueueByIndexElem<T>(elem);
+            LastElem = LastElem.nextElement;
+        }
+        public T Dequeue()
+        {
+            if (FirstElem == null)
+                throw new Exception("Blankness exception");
+
+            T elem = FirstElem.Element;
+            FirstElem = FirstElem.nextElement;
+            return elem;
+
+        }
+        public class QueueByIndexElem<T>
+        {
+            public T Element { get; set; }
+            public QueueByIndexElem<T> nextElement { get; set; }
+            public QueueByIndexElem(T elem)
+            {
+                Element = elem;
+            }
+        }
     }
 }
